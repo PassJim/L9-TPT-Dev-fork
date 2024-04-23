090d607ad789b6b7a227902078bf8e10fe976df5
Passjim <PassJim@users.noreply.github.com>
2024-04-23 12:16:45 +0100

    Create COPR.cpp

---
+++ src/simulation/elements/COPR.cpp
@@ -0,0 +1,51 @@
+#include "simulation/ElementCommon.h"
+
+//static int update(UPDATE_FUNC_ARGS);
+
+void Element::Element_COPR()
+{
+	Identifier = "DEFAULT_PT_COPR";
+	Name = "COPR";
+	Colour = 0x505070_rgb;
+	MenuVisible = 1;
+	MenuSection = SC_SOLIDS;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.90f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 1;
+	Hardness = 1;
+
+	Weight = 100;
+
+	HeatConduct = 251;
+	Description = "Breakable metal. Common conductive building material, can melt and break under pressure.";
+
+	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = 1.0f;
+	HighPressureTransition = ST;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 1273.0f;
+	HighTemperatureTransition = PT_LAVA;
+
+	//Update = &update;
+}
+
+//static int update(UPDATE_FUNC_ARGS)
+{
+
+}
